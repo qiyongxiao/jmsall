8a83b06a23e5840caa6cf04756c4cb23116971ad
qiyongxiao <qiyongxiao@magicdatatech.com>
2021-05-20 10:33:22 +0800

    优化shell

--- allinone/entrypoint.sh
+++ allinone/entrypoint.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
-if [ $DB_HOST == 127.0.0.1 ]; then
-    if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
-        mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
-        mysqld --daemonize --user=mysql
-        if [[ ! -z $(pidof mysqld) ]];then
-          sleep 5s
-          mysql -uroot -e "create database jumpserver default charset 'utf8' collate 'utf8_bin'; grant all on jumpserver.* to 'jumpserver'@'127.0.0.1' identified by '$DB_PASSWORD'; flush privileges;"
-        else
-          echo "Mysql数据库未启动"
-        fi
-    else
-        mysqld --daemonize --user=mysql
-    fi
+if [ "${DB_HOST}" == "127.0.0.1" ]; then
+	if [[ ! -d "/var/lib/mysql/${DB_NAME}" ]]; then
+		mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
+		mysqld --daemonize --user=mysql
+		if [[ ! -z $(pidof mysqld) ]]; then
+			sleep 5
+			mysql -uroot -e "create database jumpserver default charset 'utf8' collate 'utf8_bin'; grant all on jumpserver.* to 'jumpserver'@'127.0.0.1' identified by '$DB_PASSWORD'; flush privileges;"
+		else
+			echo "Mysql数据库未启动"
+			sleep 10
+		fi
+	else
+		chown -R mysql:mysql "/var/lib/mysql/${DB_NAME}"
+		mysqld --daemonize --user=mysql
+	fi
 fi
 
-if [ $REDIS_HOST == 127.0.0.1 ]; then
-    redis-server &
+if [ "$REDIS_HOST" == "127.0.0.1" ]; then
+	redis-server &
 fi
 
 if [ ! -f "/opt/jumpserver/config.yml" ]; then
-    echo > /opt/jumpserver/config.yml
+	echo >/opt/jumpserver/config.yml
 fi
 
-if [ ! $WINDOWS_SKIP_ALL_MANUAL_PASSWORD ]; then
-    export WINDOWS_SKIP_ALL_MANUAL_PASSWORD=True
+if [ ! "$WINDOWS_SKIP_ALL_MANUAL_PASSWORD" ]; then
+	export WINDOWS_SKIP_ALL_MANUAL_PASSWORD=True
 fi
 
 source /opt/py3/bin/activate
 cd /opt/jumpserver && ./jms start -d
 cd /opt/koko && ./koko -d
 /etc/init.d/guacd start
-sh /config/tomcat9/bin/startup.sh
+sh /config/tomcat10/bin/startup.sh
 /usr/sbin/nginx &
 
-echo "Jumpserver ALL $Version"
+# shellcheck disable=SC2154
+echo "Jumpserver ALL ${Version}"
 tail -f /var/log/nginx/access.log

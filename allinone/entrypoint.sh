c27aa0938361350aeb182f3644a3f91f9c277243
seaurching <1433290059@qq.com>
2021-05-22 00:26:12 +0800

    兼容MySQL8.0

--- allinone/entrypoint.sh
+++ allinone/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+
 if [ "${DB_HOST}" == "127.0.0.1" ]; then
 	if [[ ! -d "/var/lib/mysql/${DB_NAME}" ]]; then
 		mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
+		chown -R mysql:mysql "/var/log/mysql*"
+		chmod -R 0775 "/var/log/mysql*"
 		mysqld --daemonize --user=mysql
 		if [[ ! -z $(pidof mysqld) ]]; then
-			sleep 5
-			mysql -uroot -e "create database jumpserver default charset 'utf8' collate 'utf8_bin'; grant all on jumpserver.* to 'jumpserver'@'127.0.0.1' identified by '$DB_PASSWORD'; flush privileges;"
+			sleep 2
+			mysql -uroot -e "create database 'jumpserver' default charset 'utf8' collate 'utf8_bin';"
+			mysql -uroot -e "create user 'jumpserver'@'127.0.0.1' identified with mysql_native_password by '${DB_PASSWORD}';"
+			mysql -uroot -e "grant all privileges on jumpserver.* to 'jumpserver'@'127.0.0.1'; flush privileges;"
 		else
 			echo "Mysql数据库未启动"
-			sleep 10
+			sleep 5
 		fi
 	else
 		chown -R mysql:mysql "/var/lib/mysql/${DB_NAME}"
+		chmod -R 0775 "/var/log/mysql*"
 		mysqld --daemonize --user=mysql
 	fi
 fi
